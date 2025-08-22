3c76795fa775729e7ae8ec6d4df17b4dcb3224d3
Mark Nash <abacefabmicroblast@hotmail.com>
2025-08-22 14:14:31 -0700

    Harden the fluentbit config

--- install_influxdb.sh
+++ install_influxdb.sh
@@ -37,23 +37,60 @@ systemctl enable influxdb
 apt install gpg
 curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
 
+cat << EOF > /etc/fluent-bit/parsers2.conf
+[PARSER]
+    Name   meminfo
+    Format regex
+    Regex  mem_total=(?<mem_total>\d+) mem_available=(?<mem_available>\d+)
+EOF
+
+cat << EOF > /etc/fluent-bit/convert_to_int.lua
+function convert_mem_values(tag, timestamp, record)
+    record["mem_total"] = tonumber(record["mem_total"])
+    record["mem_available"] = tonumber(record["mem_available"])
+    return 1, timestamp, record
+end
+EOF
+
 cat << EOF > /etc/fluent-bit/fluent-bit.conf
+[SERVICE]
+    parsers_file parsers2.conf
+
 [INPUT]
     Name cpu
     Tag cpu
+    interval_sec 5
+    
 
 [INPUT]
-    Name mem
-    Tag memory
+    name        exec
+    command     free | awk 'NR==2 { printf "mem_total=%d mem_available=%d\n", $2, $7 }'
+    interval_sec 5
+    tag         mfm
+    parser      meminfo
+
+[FILTER]
+    Name    lua
+    Match   mfm
+    script  /etc/fluent-bit/convert_to_int.lua
+    call    convert_mem_values
+    
 
 [INPUT]
     Name netif
     Tag network
     Interface eth0
+    interval_sec 5
 
 [INPUT]
     Name disk
     Tag disk
+    interval_sec 5
+
+[OUTPUT]
+    name stdout
+    match *
+    format json_lines
 
 [OUTPUT]
     Name influxdb
